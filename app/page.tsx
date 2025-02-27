2bebc39bd06936a6bc285bb3750a50536a95eac1
Sachintha Sithuruwan <sachinthasithu123@gmail.com>
2025-02-27 20:31:13 +0530

    feat: swap button and arrow colors in demo request button

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,6 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { TbBrandTiktok } from "react-icons/tb"; // TikTok icon from react-icons
 import Link from "next/link";
-import AOS from "aos";
-import "aos/dist/aos.css";
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -170,12 +168,6 @@ export default function Home() {
   const [scrollProgress, setScrollProgress] = useState(0);
 
   useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      once: true,
-      easing: 'ease-in-out',
-      offset: 120,
-    });
     let lastScrollY = window.scrollY;
 
     const handleScroll = () => {
@@ -359,12 +351,28 @@ export default function Home() {
       <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
         Transform your fleet operations with real-time tracking, smart monitoring, and data-driven insights.
       </p>
-      <Button
-        size="lg"
-        className="text-lg px-8 py-6 bg-gradient-to-r from-[#6D6BF8] to-[#54C1D5] hover:from-[#6D6BF8]/90 hover:to-[#54C1D5]/90 text-white"
+      <button className="group relative inline-flex h-[calc(48px+8px)] items-center justify-center rounded-full bg-neutral-700 py-1 pl-6 pr-14 font-medium text-white">
+  <span className="z-10 pr-2">Request a Demo</span>
+  <div className="absolute right-1 inline-flex h-12 w-12 items-center justify-end rounded-full bg-gradient-to-r from-[#6D6BF8] to-[#54C1D5] transition-[width] group-hover:w-[calc(100%-8px)]">
+    <div className="mr-3.5 flex items-center justify-center">
+      <svg
+        width="15"
+        height="15"
+        viewBox="0 0 15 15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5 text-neutral-50"
       >
-        Request a Demo <ChevronRight className="ml-2" />
-      </Button>
+        <path
+          d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+          fill="currentColor"
+          fillRule="evenodd"
+          clipRule="evenodd"
+        ></path>
+      </svg>
+    </div>
+  </div>
+</button>
     </motion.div>
   </div>
 </section>
