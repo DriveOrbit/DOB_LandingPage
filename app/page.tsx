f8f19ac9273220383853599291b17f4a7a271cd0
Sachintha Sithuruwan <sachinthasithu123@gmail.com>
2025-03-04 11:21:42 +0530

    feat: add demo request button and enhance 'Get Started Now' button design

--- app/page.tsx
+++ app/page.tsx
@@ -373,6 +373,7 @@ export default function Home() {
       <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
         Transform your fleet operations with real-time tracking, smart monitoring, and data-driven insights.
       </p>
+      <Link href="https://youtu.be/gnYwnRNFqQ0?si=2u6aF79zeGVv-xwy">
       <button className="group relative inline-flex h-[calc(48px+8px)] items-center justify-center rounded-full bg-neutral-700 py-1 pl-6 pr-14 font-medium text-white">
   <span className="z-10 pr-2">Request a Demo</span>
   <div className="absolute right-1 inline-flex h-12 w-12 items-center justify-end rounded-full bg-gradient-to-r from-[#6D6BF8] to-[#54C1D5] transition-[width] group-hover:w-[calc(100%-8px)]">
@@ -395,6 +396,7 @@ export default function Home() {
     </div>
   </div>
 </button>
+</Link>
     </motion.div>
   </div>
 </section>
@@ -598,9 +600,28 @@ export default function Home() {
           <TypewriterEffectSmooth words={words} />
           <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
             <Link href="/form">
-              <button className="w-40 h-12 rounded-xl bg-white text-black border border-black  text-sm">
-                Get Started Now 
-              </button>
+            <button className="group relative inline-flex h-[calc(48px+8px)] items-center justify-center rounded-full bg-neutral-700 py-1 pl-6 pr-14 font-medium text-white">
+  <span className="z-10 pr-2">Get Started Now</span>
+  <div className="absolute right-1 inline-flex h-12 w-12 items-center justify-end rounded-full bg-gradient-to-r from-[#6D6BF8] to-[#54C1D5] transition-[width] group-hover:w-[calc(100%-8px)]">
+    <div className="mr-3.5 flex items-center justify-center">
+      <svg
+        width="15"
+        height="15"
+        viewBox="0 0 15 15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5 text-neutral-50"
+      >
+        <path
+          d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+          fill="currentColor"
+          fillRule="evenodd"
+          clipRule="evenodd"
+        ></path>
+      </svg>
+    </div>
+  </div>
+</button>
             </Link>
           </div>
         </div>
